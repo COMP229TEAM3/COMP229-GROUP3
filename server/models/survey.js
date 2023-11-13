a577d30bcad37d5ffa0e87ec75a704e474b44d27
wsiu11 <wsiu11@my.centennialcollege.ca>
2023-11-13 17:23:22 -0500

    add survey model

---
+++ server/models/survey.js
@@ -0,0 +1,67 @@
+
+
+
+// require modules for the Survey Model
+let mongoose = require('mongoose');
+
+let SurveyQuestion = mongoose.Schema
+(
+    {
+        questsion:
+        {
+            type: String,
+            default: "",
+            trim: true,
+            required: 'question is required'
+        },
+        answers:
+        [{
+            type: String,
+            default: "",
+            trim: true,
+            required: 'answer is required'
+        }]
+    }
+);
+
+let Survey= mongoose.Schema
+(
+    {
+        name: 
+        {
+            type: String,
+            default: "",
+            trim: true,
+            required: 'name for survey is required'
+        },
+        description: 
+        {
+            type: String,
+            default: "",
+            trim: true,
+            required: 'description is required'
+        },
+       questions:
+       {
+            type: [SurveyQuestion],
+            default: undefined,
+            required: 'questions are required'
+       },
+        created:
+        {
+            type: Date,
+            default: Date.now
+        },
+        updated:
+        {
+            type: Date,
+            default: Date.now
+
+        }
+    },    
+    {
+        collection: "surveys"
+    }
+);
+
+module.exports.Survey = mongoose.model('Survey', Survey);
