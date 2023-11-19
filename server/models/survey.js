976de654b3f65ebc9b1d6d7e5a3367a0de197d1e
wsiu11 <wsiu11@my.centennialcollege.ca>
2023-11-19 00:03:40 -0500

    update survey model

--- server/models/survey.js
+++ server/models/survey.js
@@ -1,67 +1,42 @@
-
-
-
 // require modules for the Survey Model
 let mongoose = require('mongoose');
 
-let SurveyQuestion = mongoose.Schema
-(
-    {
-        questsion:
+let Survey = mongoose.Schema
+    (
         {
-            type: String,
-            default: "",
-            trim: true,
-            required: 'question is required'
-        },
-        answers:
-        [{
-            type: String,
-            default: "",
-            trim: true,
-            required: 'answer is required'
-        }]
-    }
-);
+            name:
+            {
+                type: String,
+                default: "",
+                trim: true,
+                required: 'name for survey is required'
+            },
+            description:
+            {
+                type: String,
+                default: "",
+                trim: true,
+                required: 'description is required'
+            },
+            dueDate:
+            {
+                type: Date
+            },
+            created:
+            {
+                type: Date,
+                default: Date.now
+            },
+            updated:
+            {
+                type: Date,
+                default: Date.now
 
-let Survey= mongoose.Schema
-(
-    {
-        name: 
-        {
-            type: String,
-            default: "",
-            trim: true,
-            required: 'name for survey is required'
+            }
         },
-        description: 
         {
-            type: String,
-            default: "",
-            trim: true,
-            required: 'description is required'
-        },
-       questions:
-       {
-            type: [SurveyQuestion],
-            default: undefined,
-            required: 'questions are required'
-       },
-        created:
-        {
-            type: Date,
-            default: Date.now
-        },
-        updated:
-        {
-            type: Date,
-            default: Date.now
-
+            collection: "surveys"
         }
-    },    
-    {
-        collection: "surveys"
-    }
-);
+    );
 
 module.exports = mongoose.model('Survey', Survey);
