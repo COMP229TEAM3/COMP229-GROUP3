296cd28b55ac30118b6857204979bc27349b00dd
wsiu11 <wsiu11@my.centennialcollege.ca>
2023-12-03 17:27:16 -0500

    fix controller

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,98 +7,90 @@ let passport = require('passport');
 let userModel = require('../models/user');
 let User = userModel.User; // alias
 
-module.exports.displayHomePage = (req, res, next ) => {
-    res.render('index', {title: 'home', displayName: req.user ? req.user.displayName : ''});
+module.exports.displayHomePage = (req, res, next) => {
+    res.render('index', { title: 'home', displayName: req.user ? req.user.displayName : '' });
 }
 
-module.exports.displayAboutPage = (req, res, next ) => {
-    res.render('index', {title: 'about',displayName: req.user ? req.user.displayName : '' });
+module.exports.displayAboutPage = (req, res, next) => {
+    res.render('index', { title: 'about', displayName: req.user ? req.user.displayName : '' });
 }
 
-module.exports.displayProjectsPage = (req, res, next ) => {
-    res.render('index', {title: 'projects', displayName: req.user ? req.user.displayName : ''});
+module.exports.displayProjectsPage = (req, res, next) => {
+    res.render('index', { title: 'projects', displayName: req.user ? req.user.displayName : '' });
 }
 
-module.exports.displayServicesPage = (req, res, next ) => {
-    res.render('index', {title: 'services', displayName: req.user ? req.user.displayName : ''});
+module.exports.displayServicesPage = (req, res, next) => {
+    res.render('index', { title: 'services', displayName: req.user ? req.user.displayName : '' });
 }
 
-module.exports.displayBusinesscontactlistPage = (req, res, next ) => {
-    res.render('index', {title: 'businesscontactlist', displayName: req.user ? req.user.displayName : ''});
+module.exports.displayBusinesscontactlistPage = (req, res, next) => {
+    res.render('index', { title: 'businesscontactlist', displayName: req.user ? req.user.displayName : '' });
 }
 
-module.exports.displayContactPage = (req, res, next ) => {
-    res.render('index', {title: 'contact', displayName: req.user ? req.user.displayName : ''});
+module.exports.displayContactPage = (req, res, next) => {
+    res.render('index', { title: 'contact', displayName: req.user ? req.user.displayName : '' });
 }
 
-module.exports.displayLoginPage = (req, res, next ) => {
-    res.render('index', {title: 'login', displayName: req.user ? req.user.displayName : ''});
+module.exports.displayLoginPage = (req, res, next) => {
+    res.render('index', { title: 'login', displayName: req.user ? req.user.displayName : '' });
 }
 
 module.exports.displayListPage = (req, res, next) => {
-    res.render('index', {title: 'data', displayName: req.user ? req.user.displayName : ''});
+    res.render('index', { title: 'data', displayName: req.user ? req.user.displayName : '' });
 }
 
 module.exports.displayLoginPage = (req, res, next) => {
     //check if the user is already logged in
-    if(!req.user)
-    {
-        res.render('auth/login', 
-        {
-            title: "Login",
-            messages: req.flash('loginMessage'),
-            displayName: req.user ? req.user.displayName : ''
-        })
+    if (!req.user) {
+        res.render('auth/login',
+            {
+                title: "Login",
+                messages: req.flash('loginMessage'),
+                displayName: req.user ? req.user.displayName : ''
+            })
     }
-    else
-    {
+    else {
         return res.redirect('/');
     }
 }
 
 module.exports.processLoginPage = (req, res, next) => {
     passport.authenticate('local',
-    (err, user, info) => {
-     // server error
-     if(err)
-     {
-        return next(err);
-     }
-     // is ther a user login error?
-     if(!user)
-     {
-        req.flash('loginMessage', 'Authentication Error');
-        return res.redirect('/login');
-     }
-     req.login(user, (err) => {
-        // server error?
-        if(err)
-        {
-            return next(err);
-        }
-        return res.redirect('/data');
-     });
-    })(req, res, next);
+        (err, user, info) => {
+            // server error
+            if (err) {
+                return next(err);
+            }
+            // is ther a user login error?
+            if (!user) {
+                req.flash('loginMessage', 'Authentication Error');
+                return res.redirect('/login');
+            }
+            req.login(user, (err) => {
+                // server error?
+                if (err) {
+                    return next(err);
+                }
+                return res.redirect('/data');
+            });
+        })(req, res, next);
 }
 
-module.exports.displayRegisterPage = (req, res, next) =>{
+module.exports.displayRegisterPage = (req, res, next) => {
     //checlk if the user is not already logged in
-    if(!req.user)
-    {
+    if (!req.user) {
         res.render('auth/register',
-        {
-            title: 'Register',
-            messages: req.flash('registerMessage'),
-            displayName: req.user ? req.user.displayName : ''
-        });
+            {
+                title: 'Register',
+                messages: req.flash('registerMessage')
+            });
     }
-    else
-    {
+    else {
         return res.redirect('/');
     }
 }
 
-module.exports.processRegisterPage = (req, res, next) =>{
+module.exports.processRegisterPage = (req, res, next) => {
     // instantiate a user object
 
     let newUser = new User({
@@ -108,27 +100,24 @@ module.exports.processRegisterPage = (req, res, next) =>{
         displayName: req.body.displayName
     });
 
-    User.register(newUser, req.body.password, (err, ) => {
-        if(err)
-        {
+    User.register(newUser, req.body.password, (err,) => {
+        if (err) {
             console.log("Error: Inserting New User");
-            if(err.name == "UserExistsError")
-            {
+            if (err.name == "UserExistsError") {
                 req.flash(
                     'registerMessage',
                     'Registraion Error: User Already Exists'
                 );
                 console.log('Error: User Already Exists!')
             }
-            return res.render('auth/register', 
-            {
-                title: 'Register',
-                messages: req.flash('registerMessage'),
-                displayName: req.user ? req.user.displayName : ''
-            });
+            return res.render('auth/register',
+                {
+                    title: 'Register',
+                    messages: req.flash('registerMessage'),
+                    displayName: req.user ? req.user.displayName : ''
+                });
         }
-        else
-        {
+        else {
             // if no error exist, then registration is successful
 
             // redirect the user and authenticate them
