487b92e60a1e47259bd3944575a9977f8e1acba7
wsiu11 <wsiu11@my.centennialcollege.ca>
2023-12-07 18:02:46 -0500

    home, about page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  template: `<h1>Hello world!</h1>`,
+  imports: [
+    HomeComponent,
+    AboutComponent,
+    FontAwesomeModule,
+    RouterModule,
+    MainNavComponent
+  ],
+  template: `
+  <main class="container">
+    <app-main-nav></app-main-nav>
+    <a [routerLink]="['/']">
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'homes';
 }
---
+++ client/src/app/routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+const routeConfig: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home page'
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+        title: 'About Us'
+    }
+];
+
+export default routeConfig;
