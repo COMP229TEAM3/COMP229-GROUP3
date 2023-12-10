faffe78c5be6d9a88d367312af88b1903eab5367
riponcc22 <mhoss206@my.centennialcollege.ca>
2023-12-09 23:48:12 -0500

    Fixed incident-list table format and login page

--- src/app/incident-list/incident-list.component.ts
+++ src/app/incident-list/incident-list.component.ts
@@ -38,7 +38,20 @@ import { RouterModule } from '@angular/router';
 
       <!-- Repeatable Template Row -->
       <tr class="d-flex" *ngFor="let incident of incidentList">
-        <app-incident [incident]="incident"></app-incident>
+      <td class="text-center col-3">{{ incident.incident }}</td>
+      <td class="text-center col-2">{{ incident.description }}</td>
+      <td class="text-center col-3">{{ incident.date }}</td>
+      <td class="text-center col-2">
+        <a [routerLink]="['/data/edit', incident._id]" class="btn btn-primary btn-sm">
+          <i class="fas fa-pencil-alt"></i> Edit
+        </a>
+      </td>
+      <td class="text-center col-2">
+        <a [routerLink]="['/data/delete', incident._id]" class="btn btn-danger btn-sm">
+          <i class="fas fa-trash-alt"></i> Delete
+        </a>
+      </td>
+       
       </tr>
     </table>
   </div>
